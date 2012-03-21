6b17f4caf209fc3aab3265402df4cfffad600b1b
yenchi.lin <yenchilin@cattail-sw.com>
2012-03-21 15:20:58 -0700

    add test project
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

---
+++ test/src/com/cattailsw/nanidroid/test/SurfaceParserTest.java
@@ -0,0 +1,60 @@
+package com.cattailsw.nanidroid.test;
+
+import java.util.regex.Matcher;
+
+import android.test.AndroidTestCase;
+import com.cattailsw.nanidroid.*;
+import android.util.Log;
+
+public class SurfaceParserTest extends AndroidTestCase {
+    private static final String TAG = "SurfaceParserTest";
+	public void testAnimationIntervalParsing() {
+		String t = "animation0.interval,never";
+		Matcher m = PatternHolders.animation_interval.matcher(t);
+		assertTrue( m.matches());		
+	}
+	
+	public void testAnimationPattern1() {
+		String t = "animation0.pattern0,overlay,1201,50,117,66";
+		Matcher m = PatternHolders.animation.matcher(t);
+		assertTrue(m.matches());
+		assertEquals(9, m.groupCount());
+		printMatch(m);
+		t = "animation2.pattern0,alternativestart,(0,1,2)";
+		m = PatternHolders.animation.matcher(t);
+		assertTrue(m.matches());
+		printMatch(m);
+		m = PatternHolders.animation_alt.matcher(t);
+		assertTrue(m.matches());
+		assertEquals(3, m.groupCount());
+		assertEquals("0,1,2", m.group(3));
+	}
+	
+	public void testAnimationPattern2_Overlay() {
+		String t = "animation0.pattern2,overlay,-1";
+		Matcher m = PatternHolders.animation_base.matcher(t);
+		assertTrue( m.matches() );
+		assertNull(m.group(5));
+		t = "animation1.pattern3,overlay,-1,100";
+		m = PatternHolders.animation_base.matcher(t);
+		assertTrue( m.matches() );
+		assertEquals(5, m.groupCount());
+		assertEquals("100", m.group(5));		
+	}
+
+    private void printMatch(Matcher m) {
+	Log.d(TAG, "matcher has " + m.groupCount() + " groups");
+	for ( int i = 0; i < m.groupCount(); i++ ) {
+	    Log.d(TAG, "("+i+") => " + m.group(i));
+	}
+	try {
+	    Log.d(TAG, "("+m.groupCount()+") => " + m.group( m.groupCount()));
+	}
+	catch(Exception e) {
+
+	}
+
+    }
+
+	
+}
