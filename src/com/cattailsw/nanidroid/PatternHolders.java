464d6e4152d174b95cbb1aa69cff0c652db87b22
yenchi.lin <yenchilin@cattail-sw.com>
2012-03-20 17:25:07 -0700

    point pattern handles both point.centerX and point.kinoko.centerX types
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

--- src/com/cattailsw/nanidroid/PatternHolders.java
+++ src/com/cattailsw/nanidroid/PatternHolders.java
@@ -7,7 +7,7 @@ public class PatternHolders {
     // Patterns for parsing Surfaces.txt
     public static final Pattern element = Pattern.compile("element(\\d+),(\\w*),(\\S*),(\\d+),(\\d+)$");
     public static final Pattern collision = Pattern.compile("collision(\\d+),(\\d+),(\\d+),(\\d+),(\\d+),(\\w*)$");
-    public static final	Pattern point = Pattern.compile("point.center([xXyY]{1}),(\\d+$)");
+    public static final	Pattern point = Pattern.compile("point.(\\w*\\.*)center([xXyY]{1}),(\\d+$)");
 
     public static final	Pattern interval = Pattern.compile("(\\d+)interval,(\\S*)$");
     public static final	Pattern pattern = Pattern.compile("(\\d+)pattern(\\d?),(\\d*|-1),(\\d*),(\\w*),(\\d*),(\\d*)$");
