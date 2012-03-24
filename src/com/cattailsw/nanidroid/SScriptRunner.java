8013e5f7ba3cd8080f871a62b9460ec29cc298d0
yenchi.lin <yenchilin@cattail-sw.com>
2012-03-23 22:03:43 -0700

    end of day commit for script runner
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

--- src/com/cattailsw/nanidroid/SScriptRunner.java
+++ src/com/cattailsw/nanidroid/SScriptRunner.java
@@ -2,6 +2,7 @@ package com.cattailsw.nanidroid;
 
 import android.content.Context;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import android.view.View;
 
 public class SScriptRunner {
     private static final String TAG = "SScriptRunner";
@@ -38,7 +39,7 @@ public class SScriptRunner {
 	    mMsgQueue.add(s);
     }
     
-    private boolean isRunning = false;
+    private Boolean isRunning = false;
 
     private String msg = null;
 
@@ -49,12 +50,133 @@ public class SScriptRunner {
 	    isRunning = true;
 	}
 
+	reset();
 	msg = mMsgQueue.poll();
 	if ( msg == null ) {
-
+	    stop();
 	}
 	else {
-
+	    parseMsg();
 	}
     }
+
+    public synchronized void stop() {
+	synchronized( isRunning ) {
+	    isRunning = false;
+	}
+    }
+
+    private void reset() {
+	sync = false;
+	sakuraTalk = true;
+	sakuraMsg.setLength(0);
+	keroMsg.setLength(0);
+	msg = "";
+	charIndex = 0;
+	sakuraSurfaceId = "0";
+	keroSurfaceId = "10";
+    }
+
+    public static final long WAIT_UNIT = 50; // wait unit is 50ms
+
+    private boolean sync = false;
+    private boolean sakuraTalk = false;
+    private StringBuilder sakuraMsg = new StringBuilder();
+    private StringBuilder keroMsg = new StringBuilder();
+    private long waitTime = WAIT_UNIT;
+    private int charIndex = 0;
+
+    private String sakuraSurfaceId = "0";
+    private String keroSurfaceId = "10";
+
+    private void appendChar(char c) {
+	if ( sync ) {
+	    sakuraMsg.append(c);
+	    keroMsg.append(c);
+	}
+	else if ( sakuraTalk )
+	    sakuraMsg.append(c);
+	else
+	    keroMsg.append(c);
+    }
+
+    private void clearMsg() {
+	if ( sakuraTalk ) 
+	    sakuraMsg.setLength(0);
+	else
+	    keroMsg.setLength(0);
+    }
+
+    private void parseMsg() {
+	waitTime = WAIT_UNIT;
+	char c1, c2;
+	loop: while(true){
+	    try {
+		c1 = msg.charAt(charIndex);
+		charIndex++;
+		if ( c1 != '\\' ) {
+		    appendChar(c1);
+		    continue;
+		}
+		// so we need to check control sequence
+		c2 = msg.charAt(charIndex);
+		charIndex++;
+		switch(c2) {
+		case '0':
+		case 'h':
+		    if ( sakuraTalk != true ){
+			sakuraTalk = true;
+			sakuraMsg.setLength(0);
+		    }
+		    break;
+		case '1':
+		case 'u':
+		    sakuraTalk = false;
+		    keroMsg.setLength(0);
+		    break;
+		case 'e': // yen-e
+		    charIndex = msg.length();
+		    break loop;
+		case 'n': // new line
+		    appendChar('\n');
+		    break loop;
+		case 'c': // clear msg
+		    clearMsg();
+		    break;
+		default:
+		    break;
+		}
+	    }
+	    catch(Exception e) {
+		break;
+	    }
+	} 
+
+	updateUI();
+    }
+
+    private void updateUI() {
+	sakura.changeSurface(sakuraSurfaceId);
+	kero.changeSurface(keroSurfaceId);
+
+
+	if ( sakuraMsg.length() == 0 ){
+	    //need to set sakura balloon to none
+	    sakuraBalloon.setVisibility(View.INVISIBLE);
+	}
+	else {
+	    sakuraBalloon.setVisibility(View.VISIBLE);
+	    sakuraBalloon.setText(sakuraMsg.toString());
+	}
+
+	if ( keroMsg.length() == 0 ) {
+	    keroBalloon.setVisibility(View.INVISIBLE);
+	}
+	else {
+	    keroBalloon.setVisibility(View.VISIBLE);
+	    keroBalloon.setText(keroMsg.toString());
+	}
+
+    }
+
 }
