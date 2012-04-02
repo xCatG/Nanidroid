25f2a03be8a2215faae5d21001c71969dff11819
yenchi.lin <yenchilin@cattail-sw.com>
2012-04-02 12:35:06 -0700

    java doesn't not allow overriding of static method
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

--- src/com/cattailsw/nanidroid/BottleLogSensor.java
+++ src/com/cattailsw/nanidroid/BottleLogSensor.java
@@ -5,10 +5,24 @@ import android.content.res.AssetManager;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
+
+import com.cattailsw.nanidroid.SSTPBottleSensor.ApiException;
+import com.cattailsw.nanidroid.SSTPBottleSensor.ParseException;
+
 import android.content.Context;
 
 
 public class BottleLogSensor extends SSTPBottleSensor {
+	
+    public static LinkedList<String> getPageContent(Context ctx)
+	throws ApiException, ParseException {
+
+	LinkedList<String> results = getUrlContent(null, ctx);
+
+	return results;
+
+    }
+    
     protected static synchronized LinkedList<String> getUrlContent(String url, Context ctx) throws ApiException {
 	try {
 
