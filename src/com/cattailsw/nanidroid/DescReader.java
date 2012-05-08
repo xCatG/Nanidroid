ce4bfaaf07d31eb5836475e4d3c247bb10feed4c
yenchi.lin <yenchilin@cattail-sw.com>
2012-05-07 17:05:10 -0700

    try to restart if descript.txt is not in SJIS
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

--- src/com/cattailsw/nanidroid/DescReader.java
+++ src/com/cattailsw/nanidroid/DescReader.java
@@ -14,6 +14,7 @@ import java.util.Hashtable;
 import java.io.IOException;
 import java.io.FileReader;
 import android.os.SystemClock;
+import com.cattailsw.nanidroid.util.NarUtil;
 
 public class DescReader {
     private static final String TAG = "DescReader";
@@ -55,12 +56,37 @@ public class DescReader {
 	dbgOutput = dbg;
     }
 
+    private Charset readFirstLineForCharset(BufferedReader br) throws IOException {
+	if ( br.markSupported() == false )
+	    return Charset.forName("Shift_JIS");
+
+	br.mark(20);
+	String line = br.readLine();
+
+	if ( line.startsWith( NarUtil.UTF8_BOM ) )
+	    line = line.substring(1);
+
+	br.reset();
+	String [] cs = line.split(",");
+	if ( cs == null || cs.length != 2 )
+	    return Charset.forName("Shift_JIS");
+	if ( cs[0].contains("charset") == false )
+	    return Charset.forName("Shift_JIS");
+	
+	return Charset.forName(cs[1]);
+    }
+
     private void parse(InputStream is) throws IOException{
 	if ( getTable() == null )
 	    setTable(new Hashtable<String, String>());
-
+       
 	BufferedReader reader = null;
 	reader = new BufferedReader(new InputStreamReader(is, Charset.forName("Shift_JIS")));
+	Charset c = readFirstLineForCharset(reader);
+	if ( c.compareTo( Charset.forName("Shift_JIS") ) != 0 ) {// not SJIS
+	    reader.close();
+	    reader = new BufferedReader(new InputStreamReader(is, c ) );
+	}
 
 	readLoop(reader, getTable());
 
@@ -97,6 +123,14 @@ public class DescReader {
 
 	reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(infilePath)), 
 							  Charset.forName("Shift_JIS")));
+
+	Charset c = readFirstLineForCharset(reader);
+	if ( c.compareTo( Charset.forName("Shift_JIS") ) != 0 ) {// not SJIS
+	    reader.close();
+	    reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(infilePath)),
+							      c ) );
+	}
+
 	readLoop(reader, ret);
 
 	reader.close();
