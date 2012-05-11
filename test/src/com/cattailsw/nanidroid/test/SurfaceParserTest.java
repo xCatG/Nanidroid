519d7f7a079ea7f80f22bad68e3c4b77e6d9abe8
yenchi.lin <yenchilin@cattail-sw.com>
2012-05-11 16:49:44 -0700

    add test case for parsing multi-surface description
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

--- test/src/com/cattailsw/nanidroid/test/SurfaceParserTest.java
+++ test/src/com/cattailsw/nanidroid/test/SurfaceParserTest.java
@@ -81,5 +81,64 @@ public class SurfaceParserTest extends AndroidTestCase {
 	assertEquals("talk", m.group(2));
 
     }
+
+    public int[] getSurfaceIds(String line) {
+	if ( line.contains(",") ) {
+	    String [] ss = line.split(",");
+	    String [] idz = new String[ss.length];
+	    int [] id = new int[ss.length];
+	    for ( int i = 0; i < ss.length; i++ ) {
+		Matcher m = PatternHolders.surface_desc_ptrn.matcher(ss[i]);
+		if ( m.matches() ) {
+		    idz[i] = m.group(1);
+		    id[i] = Integer.parseInt(idz[i]);
+		}
+	    }
+	    return id;
+
+	}
+	else {
+	    Matcher m = PatternHolders.surface_desc_ptrn.matcher(line);
+	    if ( m.matches() ) {
+		return new int[]{ Integer.parseInt(m.group(1)) };
+	    }
+	    return null;
+	}
+    }
+
+    public void testSurfaceWithCommaSeparation() {
+	String t = "surface0,surface1";
+	// we need to actually create two separate surface objects
+	// first find ,
+	if ( t.contains(",") ) {
+	    String [] ss = t.split(",");
+	    String [] idz = new String[ss.length];
+	    for ( int i = 0; i < ss.length; i++ ) {
+		Matcher m = PatternHolders.surface_desc_ptrn.matcher(ss[i]);
+		if ( m.matches() )
+		    idz[i] = m.group(1);
+	    }
+	    assertEquals("0", idz[0]);
+	    assertEquals("1", idz[1]);
+	}
+
+	int[] id = getSurfaceIds(t);
+	assertEquals(0, id[0]);
+	assertEquals(1, id[1]);
+
+	t = "surface1020";
+	id = getSurfaceIds(t);
+	assertEquals(1, id.length);
+	assertEquals(1020, id[0]);
+
+	t = "surface1,"; // should choke? or should parse fine?
+	id = getSurfaceIds(t);
+	assertEquals(1, id.length);
+
+	t = "asldkas;ldkasl;";
+	id = getSurfaceIds(t);
+	assertNull(id);
+
+    }
 	
 }
