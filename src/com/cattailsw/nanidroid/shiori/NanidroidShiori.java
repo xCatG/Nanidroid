af54bfe5ddea18e331270de8f6a44d578d4d9512
yenchi.lin <yenchilin@cattail-sw.com>
2012-05-14 16:16:49 -0700

    try to come up with some more-helpful messages
    Signed-off-by: yenchi.lin <yenchilin@cattail-sw.com>

--- src/com/cattailsw/nanidroid/shiori/NanidroidShiori.java
+++ src/com/cattailsw/nanidroid/shiori/NanidroidShiori.java
@@ -17,13 +17,15 @@ public class NanidroidShiori extends EchoShiori {
     HashSet<String> igTable = new HashSet<String>();
     public static final String RES_NO_CONTENT = "SHIORI/3.0 204 NO CONTENT";
     private static final String RES_HEADER = "SHIORI/3.0 200 OK\r\nSender: EchoShiori\r\nValue: ";
+    private static final String RES_END = "\\e\\r\\n\\r\\n";
     String header = null;
 
-    Hashtable<String, String> reqTable = null;
+    //    Hashtable<String, String> reqTable = null;
     
     public NanidroidShiori() {
 
     }
+
     public void terminate() {
 
     }
@@ -33,21 +35,45 @@ public class NanidroidShiori extends EchoShiori {
     }
     
     
-	@Override
-	protected String genResponse() {
-		if ( reqTable == null )
-			return super.genResponse();
-		
-		String reqId = reqTable.get("id");
-		if ( reqId.equalsIgnoreCase("OnGhostChanging")) {
-			return  RES_HEADER + reqTable.get("id") + "\\e";
-		}
-		else if ( reqId.equalsIgnoreCase("OnBoot")) {
-			return RES_NO_CONTENT;
-		}
-		
-		return super.genResponse();
+    @Override
+    protected String genResponse() {
+	if ( reqTable == null )
+	    return super.genResponse();
+	
+	String reqId = reqTable.get("id");
+	if ( reqId.equalsIgnoreCase("OnGhostChanging")) {
+	    return  handleGhostChanging();//RES_HEADER + reqTable.get("id") + "\\e";
 	}
+	else if ( reqId.equalsIgnoreCase("OnGhostChanged")) {
+	    return handleGhostChanged();
+	}
+	else if ( reqId.equalsIgnoreCase("OnClose") ) {
+	    return handleOnClose();
+	}
+	else if ( reqId.equalsIgnoreCase("OnBoot")) {
+	    return RES_NO_CONTENT;
+	}
+	else if ( reqId.equalsIgnoreCase("OnInstallFailure") ) {
+	    return handleInstallFail();
+	}
+		
+	return RES_NO_CONTENT;
+	//	return super.genResponse();
+    }
 
-    
+    private String handleGhostChanging() {
+	return RES_HEADER + reqTable.get("reference0") + RES_END;
+    }
+
+    private String handleGhostChanged() {
+	return RES_HEADER + reqTable.get("reference0") + RES_END;
+    }
+
+    private String handleOnClose() {
+	return RES_HEADER + "OnClose" + RES_END;
+    }
+
+    private String handleInstallFail() {
+	return RES_HEADER + "Install Failed" + RES_END;
+    }
 }
