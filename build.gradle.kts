380b546d1f8627f5434740dff55fe90a92b6946c
Yenchi Lin <yenchilin@gmail.com>
2026-07-22 22:52:02 -0700

    test: characterize trusted forced-id NAR extraction

--- build.gradle.kts
+++ build.gradle.kts
@@ -231,6 +231,7 @@ val characterizationTests = listOf(
     "test/jvm/com/cattailsw/nanidroid/SurfaceDefinitionCharacterizationTest.java",
     "test/jvm/com/cattailsw/nanidroid/ViewServerLifecycleCharacterizationTest.java",
     "test/jvm/com/cattailsw/nanidroid/GhostSwitchingCharacterizationTest.java",
+    "test/jvm/com/cattailsw/nanidroid/NarArchiveCharacterizationTest.java",
 )
 val jvmTestSources = files(
     fileTree("src") {
