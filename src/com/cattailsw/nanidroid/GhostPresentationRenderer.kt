de0b9d228d2f037dd684a499227ca85facf33ebb
Yenchi Lin <yenchilin@gmail.com>
2026-07-26 21:32:23 -0700

    migrate presentation renderer contract to Kotlin

---
+++ src/com/cattailsw/nanidroid/GhostPresentationRenderer.kt
@@ -0,0 +1,6 @@
+package com.cattailsw.nanidroid
+
+/** Applies a complete Ghost presentation frame to a concrete UI technology. */
+fun interface GhostPresentationRenderer {
+    fun render(frame: GhostPresentationFrame)
+}
