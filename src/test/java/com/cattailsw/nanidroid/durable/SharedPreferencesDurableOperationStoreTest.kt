2b0f04c043ca98b88f4db402715c81a389a8be7a
Yenchi Lin <yenchilin@gmail.com>
2026-08-10 22:59:14 -0700

    test: update durable store version roundtrip expectation

--- src/test/java/com/cattailsw/nanidroid/durable/SharedPreferencesDurableOperationStoreTest.kt
+++ src/test/java/com/cattailsw/nanidroid/durable/SharedPreferencesDurableOperationStoreTest.kt
@@ -21,7 +21,7 @@ class SharedPreferencesDurableOperationStoreTest {
 
         assertTrue(store.putIfAbsent(record))
         assertEquals(record, SharedPreferencesDurableOperationStore(storage).read().single())
-        assertTrue(storage.value!!.startsWith("v5\n"))
+        assertTrue(storage.value!!.startsWith("v6\n"))
     }
 
     @Test fun emptyPresentValueCorruptionIsQuarantinedAndReadsAreBlockedUntilRecovery() {
